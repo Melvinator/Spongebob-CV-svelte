8346836a78c4dbe4875b23e66cc7c5521f903aac
Melvinator <122350522+Melvinator@users.noreply.github.com>
2023-05-02 08:22:16 +0000

    add html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,92 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/testRoute">Go to testRoute</a>
+<div class="readable-area">
+    <a class="header" href="/photo.html">
+        <div class="header-text">
+            <h1> Spongebob, Squarepants </h1>
+            <p class="job-title"> BOSS Chef </p>
+        </div>
+        <img src="https://i.pinimg.com/originals/c4/96/9a/c4969aaedbc096c09b35e31abd11e2ec.png"
+            alt="profile icture">
+    </a>
+    <div class="content">
+        <div class="content-column">
+            <div class="section-title">
+                <i class="fa-solid fa-user title-icon">
+                </i>
+                Profile
+            </div>
+            <p>
+                BOZO dude
+            </p>
+            <div class="section-title">
+                <i class="fa-solid fa-book title-icon"></i>
+                Education
+            </div>
+            <ul>
+                <li>
+                    <span>672 - 1965</span>
+                    <span class="place">Bikini Bottom So low School</span>
+                </li>
+                <li>
+                    <span>1965 - 2074</span>
+                    <span class="place">Bikini Bottom So high School</span>
+                </li>
+            </ul>
+        </div>
+        <div class="content-column">
+            <div class="section-title">
+                <i class="fa-solid fa-book title-icon"></i>
+                Education
+            </div>
+            <ul>
+                <li>
+                    <span>2074 - 2259</span>
+                    <span class="place">Bikini Bottom and top Too High School</span>
+                </li>
+                <li>
+                    <span>2259 - 5561</span>
+                    <span class="place">Bikini Bottom, top, left, and right Too High School</span>
+                </li>
+            </ul>
+            <div class="section-title">
+                <i class="fa-solid fa-wand-sparkles title-icon"></i>
+                Skills
+            </div>
+            <ul>
+
+                <li class="skill">
+                    <span>"Money" Hunting</span>
+                    <div class="gauge">
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                    </div>
+                </li>
+                <li class="skill">
+                    <span>"Waifu" Hunting</span>
+                    <div class="gauge">
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                    </div>
+                </li>
+                <li class="skill">
+                    <span>"Actual" Hunting</span>
+                    <div class="gauge">
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet filled-bullet"></div>
+                        <div class="bullet"></div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
