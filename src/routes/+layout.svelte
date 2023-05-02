940f6230cc10d900b40859ac13538842937019a0
Melvinator <122350522+Melvinator@users.noreply.github.com>
2023-05-02 08:14:55 +0000

    setup font & bg color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,26 +18,16 @@
 </div>
 
 <style>
+    :global(*) {
+        padding : 0;
+        margin: 0;
+        font-family: "open sans";
+    }
     :global(html, body) {
         width: 100%;
         height: 100%;
-        padding : 0;
-        margin: 0;
+        background: #fffde7;
     }
-    .parent {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: row;
-    }
-    .sidebar {
-        background-color:blue;
-        width: 200px;
-        height:100%;
-    }
-    .content {
-        display: flex;
-        flex-direction: column;
-    }
+   
     
 </style>
